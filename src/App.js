70a86d323fcf7d7fba8c56aab4bf70c32ec187a5
Aiden Cullo <aidencullo@gmail.com>
2024-09-18 17:03:04 -0400

    two increments don't work

--- src/App.js
+++ src/App.js
@@ -1,46 +1,32 @@
-import { useState } from 'react';
-import { sculptureList } from './data.js';
+import { useState } from "react";
+import { sculptureList } from "./data.js";
 
 export default function Gallery() {
   const [index, setIndex] = useState(0);
 
-  function handleNext() {
-    if (index === sculptureList.length - 1) {
-      return;
-    }
-    setIndex(a => a + 1);
+  function nextIndex() {
+    setIndex(index + 1);
   }
 
-  function handlePrev() {
-    if (index === 0) {
-      return;
-    }
-    setIndex(i => i - 1);
+  function nextNextIndex() {
+    setIndex(index + 1);
+    setIndex(index + 1);
   }
 
   let sculpture = sculptureList[index];
   return (
     <>
-      <button onClick={handleNext}>
-        Next
-      </button>
-      <button onClick={handlePrev}>
-        Prev
-      </button>
+      <button onClick={nextIndex}>Next</button>
+      <button onClick={nextNextIndex}>Next++</button>
       <h2>
-        <i>{sculpture.name} </i> 
+        <i>{sculpture.name} </i>
         by {sculpture.artist}
       </h2>
-      <h3>  
+      <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
-      <img 
-        src={sculpture.url} 
-        alt={sculpture.alt}
-      />
-      <p>
-        {sculpture.description}
-      </p>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <p>{sculpture.description}</p>
     </>
   );
 }
